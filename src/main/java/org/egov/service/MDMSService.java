e4efbe35437e4ae2fddd182c97c11d5346fa2829
Limark Dcunha <83493294+limarkdcunha@users.noreply.github.com>
2023-08-15 21:24:51 +0530

    Integrated persister service

--- src/main/java/org/egov/service/MDMSService.java
+++ src/main/java/org/egov/service/MDMSService.java
@@ -8,8 +8,8 @@ import digit.models.coremodels.mdms.MasterDetail;
 import digit.models.coremodels.mdms.MdmsCriteriaReq;
 import digit.models.coremodels.mdms.ModuleDetail;
 import lombok.extern.slf4j.Slf4j;
-import net.minidev.json.JSONValue;
 import org.egov.models.MDMSData;
+import org.egov.models.MDMSNewRequest;
 import org.egov.models.MDMSRequest;
 import org.egov.repository.MDMSRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,18 +29,24 @@ public class MDMSService {
     @Autowired
     private MDMSRepository repository;
 
+    @Autowired
+    private Producer producer;
+
+    @Autowired
+    private MDMSConfig config;
+
     public Map<String, Map<String, JSONArray>> searchMaster(MdmsCriteriaReq mdmsCriteriaReq) {
         String tenantId = mdmsCriteriaReq.getMdmsCriteria().getTenantId();
-        log.info("tenantId"+tenantId);
+        log.info("tenantId" + tenantId);
 
         List<ModuleDetail> moduleDetails = mdmsCriteriaReq.getMdmsCriteria().getModuleDetails();
-        log.info("moduleDetails"+moduleDetails);
+        log.info("moduleDetails" + moduleDetails);
 
         Map<String, Map<String, JSONArray>> responseMap = new HashMap<>();
 
         for (ModuleDetail moduleDetail : moduleDetails) {
             List<MasterDetail> masterDetails = moduleDetail.getMasterDetails();
-            log.info("masterDetails"+masterDetails);
+            log.info("masterDetails" + masterDetails);
 
             Map<String, JSONArray> finalMasterMap = new HashMap<>();
 
@@ -48,8 +54,9 @@ public class MDMSService {
                 JSONArray masterData = null;
 
                 try {
-                    //DB call
-                    masterData = getMasterDataFromDatabase(tenantId, moduleDetail.getModuleName(), masterDetail.getName());
+                    // DB call
+                    masterData = getMasterDataFromDatabase(tenantId, moduleDetail.getModuleName(),
+                            masterDetail.getName());
                 } catch (Exception e) {
                     throw new RuntimeException("Exception occurred while reading master data", e);
                 }
@@ -69,13 +76,13 @@ public class MDMSService {
 
     private JSONArray getMasterDataFromDatabase(String tenantId, String moduleName, String masterName) {
 
-        //Fetching master data object from database
-        MDMSData data = repository.findByTenantIdAndModuleNameAndMasterName(tenantId,moduleName,masterName);
+        // Fetching master data object from database
+        MDMSData data = repository.findByTenantIdAndModuleNameAndMasterName(tenantId, moduleName, masterName);
 
-        //Fetching the master data from the object
+        // Fetching the master data from the object
         JsonNode masterData = data.getMasterData();
 
-        //Converting JSON-node to JSONArray for backward compatibility
+        // Converting JSON-node to JSONArray for backward compatibility
         String jsonString = masterData.toString();
         Object parsedObject = JSONValue.parse(jsonString);
 
@@ -92,13 +99,13 @@ public class MDMSService {
     }
 
     @CacheEvict(value = "mdmsDataCache", allEntries = true)
-    public MDMSData saveMDMSData(MDMSRequest request) {
-
+    public MDMSData saveMDMSData(MDMSNewRequest newRequest) {
+        MDMSData request = newRequest.getMdmsData();
         try {
-            //Appending master data to existing data using master name
+            // Appending master data to existing data using master name
             String masterName = request.getMasterName();
 
-            //Fetching existing master data Object
+            // Fetching existing master data Object
             MDMSData existingMasterDataObject = getMDMSDataByMasterName(masterName);
 
             // If no master data is present create a new one
@@ -110,13 +117,14 @@ public class MDMSService {
                 object.setMasterName(request.getMasterName());
                 object.setMasterData(request.getMasterData());
 
-                return repository.save(object);
+                producer.push(config.getSaveMDMDSDataTopic(), newRequest);
+                // return repository.save(object);
             }
 
-            //Fetching new master data to be appended
+            // Fetching new master data to be appended
             JsonNode newMasterDataObject = request.getMasterData();
 
-            //Fetching existing master data
+            // Fetching existing master data
             JsonNode existingMasterData = existingMasterDataObject.getMasterData();
 
             if (existingMasterData.isArray() && newMasterDataObject.isArray()) {
@@ -124,19 +132,18 @@ public class MDMSService {
                 // Casting the existing master data to ArrayNode (assuming it is an array)
                 ArrayNode existingMasterDataArrayNode = (ArrayNode) existingMasterData;
 
-                //Looping through new master data
+                // Looping through new master data
                 for (JsonNode element : newMasterDataObject) {
                     // //Appending the object to existing master data
                     existingMasterDataArrayNode.add(element);
                 }
 
-                //Setting the master data in MDMS request body
+                // Setting the master data in MDMS request body
                 existingMasterDataObject.setMasterData(existingMasterDataArrayNode);
                 log.info("existingMasterDataObject: " + existingMasterDataObject);
             }
             return repository.save(existingMasterDataObject);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             log.error("Error while saving MDMSData: " + e.getMessage(), e);
             throw new RuntimeException("Error while saving MDMSData", e);
         }
@@ -145,33 +152,31 @@ public class MDMSService {
     @Cacheable(value = "mdmsDataCache")
     public List<MDMSData> getMDMSData() {
 
-        try{
+        try {
             return repository.findAll();
-        }catch (Exception e) {
+        } catch (Exception e) {
             log.error("Error while fetching MDMSData: " + e.getMessage(), e);
             throw new RuntimeException("Error while fetching MDMSData", e);
         }
 
     }
 
-    @Cacheable( value = "mdmsDataCache",key = "#id")
+    @Cacheable(value = "mdmsDataCache", key = "#id")
     public MDMSData getMDMSDataById(int id) {
         try {
             return repository.findById(id).orElse(null);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             log.error("Error while fetching MDMSData by ID: " + e.getMessage(), e);
             throw new RuntimeException("Error while fetching MDMSData by ID", e);
         }
     }
 
-    @Cacheable(value = "mdmsDataCache",key = "#masterName")
-    public MDMSData getMDMSDataByMasterName(String masterName){
+    @Cacheable(value = "mdmsDataCache", key = "#masterName")
+    public MDMSData getMDMSDataByMasterName(String masterName) {
 
         try {
             return repository.findByMasterName(masterName);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             log.error("Error while fetching MDMSData by Master Name: " + e.getMessage(), e);
             throw new RuntimeException("Error while fetching MDMSData by Master Name", e);
         }
@@ -184,14 +189,14 @@ public class MDMSService {
             MDMSData existingData;
             try {
                 existingData = repository.findById(request.getId()).orElse(null);
-            }catch (Exception e) {
+            } catch (Exception e) {
                 log.error("Error while finding MDMSData for updating: " + e.getMessage(), e);
                 throw new RuntimeException("Error while finding MDMSData for updating", e);
             }
 
             try {
                 existingData.setTenantId(request.getTenantId());
-            }catch (Exception e) {
+            } catch (Exception e) {
                 log.error("Error while finding MDMSData for updating: " + e.getMessage(), e);
                 throw new RuntimeException("Error while finding MDMSData for updating", e);
             }
@@ -201,21 +206,19 @@ public class MDMSService {
             existingData.setMasterData(request.getMasterData());
 
             return repository.save(existingData);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             log.error("Error while updating MDMSData: " + e.getMessage(), e);
             throw new RuntimeException("Error while updating MDMSData", e);
         }
     }
 
-    @CacheEvict( value = "mdmsDataCache",allEntries = true)
+    @CacheEvict(value = "mdmsDataCache", allEntries = true)
     public String deleteMDMSData(int id) {
 
         try {
             repository.deleteById(id);
             return "Request removed !! " + id;
-        }
-         catch (Exception e) {
+        } catch (Exception e) {
             log.error("Error while deleting MDMSData: " + e.getMessage(), e);
             throw new RuntimeException("Error while deleting MDMSData", e);
         }
